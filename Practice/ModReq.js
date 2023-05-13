ee1f0999ec1c8de6496a1ebc36af207be026a13a
mrtaz77 <mrtazwar23@gmail.com>
2023-05-13 20:18:13 +0600

    Module and Require

---
+++ Practice/ModReq.js
@@ -0,0 +1,7 @@
+var count = function(arr){
+    return arr.length + ' elements in this array'
+};
+
+// console.log(count(['a', 'b', 'c']))
+
+module.exports = count
---
+++ Practice/ReqCount.js
@@ -0,0 +1,3 @@
+var count = require('./ModReq')
+
+console.log(count(['a', 'b', 'c', 'd', 'e', 'f']))
