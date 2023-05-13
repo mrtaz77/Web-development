8903112ecb2722e4016ecfc09e549cb1a4872f9e
mrtaz77 <mrtazwar23@gmail.com>
2023-05-14 00:59:02 +0600

    Module Patterns

---
+++ Practice/ReturnETCUtil.js
@@ -0,0 +1,5 @@
+var ret = require('./ReturnEtc')
+
+console.log(ret.count(['a', 'b', 'c']))
+console.log(ret.adder(2,8))
+console.log(ret.adder(ret.pi, 8))
---
+++ Practice/ReturnEtc.js
@@ -0,0 +1,10 @@
+module.exports.count = function(arr){
+    return arr.length + ' elements in this array'
+};
+
+module.exports.adder = function(a,b){
+    return 'Sum = '+(a+b)
+}
+
+module.exports.pi = 3.1416
+
