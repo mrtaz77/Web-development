7653039a80ba6cddbb34339a3c30ed7f772edb4c
mrtaz77 <mrtazwar23@gmail.com>
2023-05-22 00:34:12 +0600

    first html

---
+++ Practice/Server/htmlserv.js
@@ -0,0 +1,15 @@
+var http = require('http');
+var fs = require('fs');
+
+
+
+var server  = http.createServer(function(req,res){
+    //response headers
+    console.log("Request: " + req.url);
+    res.writeHead(200,{'Content-Type': 'text/html'});
+    var myReadStream = fs.createReadStream(__dirname + '/index.html','utf8')
+    myReadStream.pipe(res);
+})
+
+server.listen(3222,'127.2.3.1')
+console.log('Listening on to port 3222')
